4703b9d6d298c948904dca481819d9b5ba7db3eb
asyronen <asy.ronen@appsflyer.com>
2020-07-29 12:41:43 +0300

    add desired outcome code generated form protoc.

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	//p := &product.Product{
+	//	BaseMsg: &base.BaseMessage {
+	//		MessageId: 12345,
+	//	},
+	//	MessageName: "IPhone X",
+	//}
+	//
+	//bytes, err := proto.Marshal(p)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//p2 := &product.Product{}
+	//err = proto.Unmarshal(bytes, p2)
+	//if err != nil {
+	//	log.Fatal(err)
+	//}
+	//
+	//fmt.Println(p2)
+	fmt.Println("done.")
+}
